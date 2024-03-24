a20cb3a8bb8dc36ccf6c7b0853646a5dc6aa7c94
jyotiraditya2005 <jyotiradityasinghbaghel30@gmail.com>
2024-03-24 22:52:26 +0530

    chal rha hai

---
+++ resp.js
@@ -0,0 +1,22 @@
+
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar background h-nav-resp')
+navlist = document.querySelector('.nav-list')
+rightnav = document.querySelector('.rightnav')
+
+
+
+burger.addEventListener('click',()=>{
+   console.log("hello")
+//    rightnav.classlist.toggle('v-class-resp');
+//     navlist.classlist.toggle('v-class-resp');
+//     navbar.classlist.toggle('h-nav-resp');
+
+})
+
+
+navbar.addEventListener('click',()=>{
+    console.log("hello")
+ 
+ })
+ 
